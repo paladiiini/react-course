7c05d65d24148d24073664890d9a10d1a85bf2ee
paladiini <paladinivitor.vp@gmail.com>
2024-01-19 09:16:56 -0300

    feat: add Login functionality to Dashboard page

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter ,Router, Routes as Switch, Navigate, Route } from 'react-router-dom';
-import { Dashboard } from '../pages';
+import { Dashboard, Login } from '../pages';
 
 export const Routes = () => {
     return (
         <BrowserRouter>
             <Switch>
             <Route path="/home" element={<Dashboard />}  />
+            <Route path="/login" element={<Login />}  />
             <Route path="/" element={<Navigate to="/home" />}  />
         </Switch>
         </BrowserRouter>
